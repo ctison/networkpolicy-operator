c398fc4500a02a7c4f904d4ead7c3c4a48ccfc56
Charles Tison <ctison@pm.me>
2020-09-09 10:32:51 +0200

    feat: disable webhooks when ENABLE_WEBHOOKS is false

--- main.go
+++ main.go
@@ -58,9 +58,11 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "NetworkPolicy")
 		os.Exit(1)
 	}
-	if err = (&networkingv1alpha1.NetworkPolicy{}).SetupWebhookWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create webhook", "webhook", "NetworkPolicy")
-		os.Exit(1)
+	if os.Getenv("ENABLE_WEBHOOKS") != "false" {
+		if err = (&networkingv1alpha1.NetworkPolicy{}).SetupWebhookWithManager(mgr); err != nil {
+			setupLog.Error(err, "unable to create webhook", "webhook", "NetworkPolicy")
+			os.Exit(1)
+		}
 	}
 	// +kubebuilder:scaffold:builder
 
